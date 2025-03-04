4c0394bf0d25063c3a1d1d24769eae3d3ef6c4ab
ygdrasil-io <alexandre.mommers@gmail.com>
2025-03-04 15:03:13 +0100

    Integrate extra IDL types and improve file handling.
    Added support for merging extra IDL types using `SequenceInputStream`. Simplified file downloading logic with a reusable `downloadToPath` function and improved naming cleanup with a `fixName` utility. Removed redundant download and handling code for better clarity.

--- generator/src/main/kotlin/main.kt
+++ generator/src/main/kotlin/main.kt
@@ -1,9 +1,22 @@
 import de.fabmax.webidl.model.IdlModel
 import de.fabmax.webidl.parser.WebIdlParser
+import java.io.SequenceInputStream
+import java.net.URI
 import java.net.URL
 import java.nio.file.Files
+import java.nio.file.Path
 import java.nio.file.Paths
-import kotlin.text.contains
+
+val idlExtraTyps = """
+    interface mixin NavigatorGPU {
+    };
+
+    interface Navigator {
+    };
+
+    interface WorkerNavigator {
+    };
+""".byteInputStream()
 
 val webUnwantedTypes = setOf(
     // Types de navigateur
@@ -40,56 +53,61 @@ val webUnwantedTypes = setOf(
 val baseSourcePath = Paths.get("webgpu-ktypes").resolve("src").resolve("commonMain").resolve("kotlin")
 val typesPath = baseSourcePath.resolve("types.kt")
 
+
+val webgpuHtmlPath = Paths.get("webgpu.html")
+val webgpuHtmlUrl = URI("https://www.w3.org/TR/webgpu/").toURL()
+val webgpuIdlPath = Paths.get("webgpu.idl")
+val webgpuIdlUrl = URI("https://gpuweb.github.io/gpuweb/webgpu.idl").toURL()
+
 fun main() {
     // Charger le fichier à partir des ressources
-    val resource = {}.javaClass.getResource("/webgpu.idl")
-        ?:error("Failed to load webgpu.idl from resources")
+    val resource = webgpuIdlUrl
+    webgpuHtmlUrl.downloadToPath(webgpuHtmlPath)
+    webgpuIdlUrl.downloadToPath(webgpuIdlPath)
 
     // Parse le contenu du fichier avec `WebIdlParser`
-    val model = WebIdlParser.parseFromInputStream(resource.openStream())
+    val model = WebIdlParser.parseFromInputStream(
+        SequenceInputStream(idlExtraTyps, resource.openStream())
+    )
 
-    //model..listTypes().joinToString(",").let { println(it) }
-
-    //downloadWebgpuDoc()
+    //model.listTypes().joinToString(",").let { println(it) }
 
     typesPath.toFile().apply {
+        delete()
         createNewFile()
+
+        model.interfaces
+            .forEach {
+                appendText("interface ${it.name.fixName()}\n")
+            }
+
+        model.dictionaries
+            .forEach {
+                appendText("interface ${it.name.fixName()}\n")
+            }
+
     }
 }
+
+private fun URL.downloadToPath(paths: Path) {
+    runCatching {
+        openStream().use { inputStream ->
+            Files.newOutputStream(paths).use { outputStream ->
+                inputStream.copyTo(outputStream)
+            }
+        }
+    }.onFailure { error(it)}
+}
+
+private fun String.fixName(): String = (if (contains(':')) substringBefore(':') else this)
+    .replace("\n", "")
+    .trim()
 
 private fun IdlModel.listTypes(): Set<String> = (enums.map { it.name } +
         interfaces.map { it.name } +
         typeDefs.map { it.name } +
         dictionaries.map { it.name })
-    .map { if (it.contains(':')) it.substringBefore(':') else it }
-    .map { it.replace("\n", "").trim() }
+    .map { it.fixName() }
     .filter { it !in webUnwantedTypes }
     .toSet()
-
-fun downloadWebgpuDoc() {
-    // Télécharger le fichier depuis l'URL
-    val url = "https://www.w3.org/TR/webgpu/"
-    val filePath = Paths.get("webgpu.html")
-
-    runCatching {
-        URL(url).openStream().use { inputStream ->
-            Files.newOutputStream(filePath).use { outputStream ->
-                inputStream.copyTo(outputStream)
-            }
-        }
-
-        println("Downloaded at : ${filePath.toAbsolutePath()}")
-    }
-
-    try {
-        URL(url).openStream().use { inputStream ->
-            Files.newOutputStream(filePath).use { outputStream ->
-                inputStream.copyTo(outputStream)
-            }
-        }
-        println("Fichier téléchargé et enregistré sous : ${filePath.toAbsolutePath()}")
-    } catch (e: Exception) {
-        println("Erreur lors du téléchargement ou de l'enregistrement du fichier : ${e.message}")
-    }
-}
 
