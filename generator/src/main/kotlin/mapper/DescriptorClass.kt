e8725c047d37e4baf61cc9dbf50b4f4cea44347c
Alexandre Mommers <alexandre.mommers@gmail.com>
2025-03-09 19:00:58 +0100

    Refactor descriptor loading to include inherited members
    Updated `loadDescriptor` to use `getMembers` for retrieving both direct and inherited members. Added `getMembers` and `getGhostMembers` functions to handle member inheritance and ghost members extraction. This ensures more comprehensive descriptor mapping and prepares for future library fixes.

--- generator/src/main/kotlin/mapper/DescriptorClass.kt
+++ generator/src/main/kotlin/mapper/DescriptorClass.kt
@@ -5,6 +5,7 @@ import MapperContext
 import fixName
 import webUnwantedTypes
 import de.fabmax.webidl.model.IdlDictionary
+import de.fabmax.webidl.model.IdlMember
 import de.fabmax.webidl.model.IdlSimpleType
 
 fun MapperContext.loadDescriptors() {
@@ -14,7 +15,7 @@ fun MapperContext.loadDescriptors() {
 }
 
 internal fun MapperContext.loadDescriptor(name: String, idlDictionary: IdlDictionary) {
-    val parameters = idlDictionary.members.map {
+    val parameters = getMembers(idlDictionary).map {
         var value = it.defaultValue
         println("$name ${it.type}")
         var type = if(it.type is IdlSimpleType) it.type.toKotlinType() else {
@@ -23,7 +24,10 @@ internal fun MapperContext.loadDescriptor(name: String, idlDictionary: IdlDictio
         }
 
         when {
-            value == null -> {}
+            value == null -> if (it.isRequired.not()) {
+                value = "null"
+                type += "?"
+            }
             value == "{}" -> {
                 value = "${type.removePrefix("GPU")}()"
             }
@@ -54,4 +58,34 @@ private fun String?.isUnsignedNumericType(type: String): Boolean = this?.let {
 private fun String.isHexadecimal(): Boolean {
     val hexRegex = Regex("^0[xX][0-9a-fA-F]+\$")
     return hexRegex.matches(this)
+}
+
+fun MapperContext.getMembers(idlDictionary: IdlDictionary): List<IdlMember> {
+    return idlDictionary.members +
+            idlDictionary.superDictionaries.flatMap { getMembers(idlModel.dictionaries.find { dictionary -> dictionary.name == it }!!) } +
+            getGhostMembers(idlDictionary)
+
+}
+
+/**
+ * To remove when fixe on parsing library
+ * Retrieves a list of ghost members related to the given `IdlDictionary`.
+ *
+ * Ghost members are those members derived from the `name` property of the provided `IdlDictionary`.
+ * If `name` contains a colon (":"), a subset of types is parsed from it, excluding unwanted types,
+ * and their corresponding members are fetched recursively.
+ *
+ * @param idlDictionary The `IdlDictionary` from which ghost members are extracted.
+ * @return A list of `IdlMember` objects representing the ghost members. Returns an empty list if `name` does not contain a colon or there are no valid ghost members.
+ */
+fun MapperContext.getGhostMembers(idlDictionary: IdlDictionary): List<IdlMember> {
+    return idlDictionary.name.takeIf { it.contains(":") }
+        ?.let {
+            it.substringAfter(":")
+                .split(",")
+                .map { it.trim() }
+                .filter { it !in webUnwantedTypes }
+                .also { println("Ghost members for ${idlDictionary.name}: $it") }
+                .flatMap { getMembers(idlModel.dictionaries.find { dictionary -> dictionary.name.fixName() == it } ?: error("Ghost member not found: $it")) }
+        } ?: emptyList()
 }
