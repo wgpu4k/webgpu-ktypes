3c68ebd79e23ca5998e12804f5c1c11f81fc594f
ygdrasil-io <alexandre.mommers@gmail.com>
2025-03-11 13:36:33 +0100

    Add deprecated typealiases for backwards compatibility.
    Introduced typealiases marked as @Deprecated to provide transitional support for renamed types. This helps maintain compatibility while encouraging migration to the new type names.

---
+++ webgpu-ktypes-descriptors/src/commonMain/kotlin/deprecated.types.kt
@@ -0,0 +1,8 @@
+package io.ygdrasil.webgpu
+
+@Deprecated("Use Extent3D instead")
+typealias Size3D = Extent3D
+@Deprecated("Use TexelCopyTextureInfo instead")
+typealias ImageCopyTexture = TexelCopyTextureInfo
+@Deprecated("Use GPUAddressMode instead")
+typealias ImageCopyBuffer = TexelCopyBufferInfo
