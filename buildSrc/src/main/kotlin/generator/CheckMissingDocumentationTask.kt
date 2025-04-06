7dcb110e127b5038375c19e76dddfa51cf531a04
ygdrasil-io <alexandre.mommers@gmail.com>
2025-04-06 22:36:56 +0200

    Add new WebGPU API documentation and improve missing checks
    Expanded documentation.json with additional WebGPU API definitions, including GPUSamplerDescriptor and GPUCommandsMixin. Revised CheckMissingDocumentationTask to adjust output for better handling of missing keys.

--- buildSrc/src/main/kotlin/generator/CheckMissingDocumentationTask.kt
+++ buildSrc/src/main/kotlin/generator/CheckMissingDocumentationTask.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.runBlocking
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
-class CheckMissingDocumentationTask : DefaultTask() {
+open class CheckMissingDocumentationTask : DefaultTask() {
 
     init {
         group = "generator"
@@ -25,6 +25,6 @@ class CheckMissingDocumentationTask : DefaultTask() {
             .map { it to it.getDocumentationKeys() }
             .filter { (_, expectedKeys) -> expectedKeys.filter { it in currentDocumentation.keys }.size != expectedKeys.size}
 
-        println("Missing keys:\n$missingKeys")
+        println("Missing keys:\n${missingKeys.flatMap { it.second }}")
     }
 }
