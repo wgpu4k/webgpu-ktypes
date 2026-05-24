47be9ec10a6e1c9f46396e211bfea67aee9cf9b2
ygdrasil-io <alexandre.mommers@gmail.com>
2026-05-24 19:04:35 +0200

    Add local const golden outputs

--- wgsl/tests/src/jvmTest/kotlin/io/ygdrasil/wgsl/tests/GoldenCompletenessTest.kt
+++ wgsl/tests/src/jvmTest/kotlin/io/ygdrasil/wgsl/tests/GoldenCompletenessTest.kt
@@ -11,7 +11,7 @@ class GoldenCompletenessTest : FunSpec({
 
     test("all missing golden outputs are explicitly documented") {
         inputNames.size shouldBe 160
-        documentedMissingOutputs.size shouldBe 43
+        documentedMissingOutputs.size shouldBe 42
 
         GoldenCorpus.backends.forEach { backend ->
             GoldenCorpus.missingOutputs(backend, rootDir) shouldBe documentedMissingOutputs
@@ -29,6 +29,6 @@ class GoldenCompletenessTest : FunSpec({
     }
 
     test("expected golden failures are validated and versioned") {
-        GoldenExpectedFailures.load(rootDir).size shouldBe 701
+        GoldenExpectedFailures.load(rootDir).size shouldBe 695
     }
 })
