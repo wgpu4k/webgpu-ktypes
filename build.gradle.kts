7572cf12b9f2faf62abdba3aab504326ee2900a3
Alexandre Mommers <919982+ygdrasil-io@users.noreply.github.com>
2026-07-16 17:55:20 +0200

    Migrate publication to Maven Central (#102)
    * build: migrate publishing to Vanniktech
    
    * ci: publish snapshots and releases to Maven Central
    
    * ci: validate publication versions
    
    * ci: preserve publication secret names

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,10 @@
-
 plugins {
-    alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotest) apply false
-    alias(libs.plugins.ksp) apply false
     generator
 }
 
 allprojects {
     group = "io.ygdrasil"
-    version = System.getenv("VERSION")?.takeIf { it.isNotBlank() } ?: "0.0.9-SNAPSHOT"
+    version = (findProperty("releaseVersion") as? String)
+        ?.takeIf { it.isNotBlank() }
+        ?: "0.0.10-SNAPSHOT"
 }
--- buildSrc/src/main/kotlin/publish.gradle.kts
+++ buildSrc/src/main/kotlin/publish.gradle.kts
@@ -1,101 +1,45 @@
-import publish.PublishingType
-import publish.centralPortalPublish
-
 plugins {
-    `maven-publish`
-    signing
+    id("com.vanniktech.maven.publish")
     id("org.jetbrains.dokka")
 }
 
-
 val libraryDescription = "Webgpu types to kotlin multiplatform generated from specifications"
 
-val signingKey = System.getenv("GPG_SECRET_KEY")
-val signingPassword = System.getenv("GPG_PASSPHRASE")
+val isPublishing = project.findProperty("signingInMemoryKey")?.toString()?.isNotBlank() == true ||
+    project.findProperty("signing.keyId")?.toString()?.isNotBlank() == true
 
-if (!isSnapshot()) {
-    signing {
-        useInMemoryPgpKeys(signingKey, signingPassword)
-        sign(publishing.publications)
+mavenPublishing {
+    if (isPublishing) {
+        publishToMavenCentral()
+        signAllPublications()
     }
-}
 
-project.centralPortalPublish {
-    username = System.getenv("MAVENCENTRAL_USERNAME")
-    password = System.getenv("MAVENCENTRAL_PASSWORD")
-    publishingType = PublishingType.USER_MANAGED
-    url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
-}
+    coordinates(group.toString(), project.name, version.toString())
 
-val javadocJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("javadoc")
-    from(tasks.findByName("dokkaGenerateHtml"))
-}
+    pom {
+        name.set(project.name)
+        description.set(libraryDescription)
+        url.set("https://github.com/wgpu4k/webgpu-ktypes")
+        inceptionYear.set("2025")
 
-publishing {
-    publications {
-        withType<MavenPublication> {
-            artifact(javadocJar)
-            pom {
-                name.set(project.name)
-                description.set(libraryDescription)
-                url.set("https://github.com/wgpu4k/webgpu-ktypes")
-                inceptionYear.set("2025")
-                licenses {
-                    license {
-                        name.set("MIT")
-                        url.set("https://opensource.org/license/MIT")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("amommers")
-                        name.set("Alexandre Mommers")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://github.com/wgpu4k/webgpu-ktypes.git")
-                    developerConnection.set("scm:git:https://github.com/wgpu4k/webgpu-ktypes.git")
-                    url.set("https://github.com/wgpu4k/webgpu-ktypes")
-                }
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://opensource.org/license/MIT")
             }
         }
-    }
 
-
-    repositories {
-        maven {
-            if (isSnapshot()) {
-                logger.info("publishing is configure as snapshot")
-                name = "GitLab"
-                url = uri("https://gitlab.com/api/v4/projects/25805863/packages/maven")
-                credentials(HttpHeaderCredentials::class) {
-                    name = "Authorization"
-                    value = "Bearer ${System.getenv("GITLAB_TOKEN")}"
-                }
-                authentication {
-                    create<HttpHeaderAuthentication>("header")
-                }
-            } else {
-                name = "Local"
-                logger.info("publishing is configure as release")
-                url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
-                logger.info("publishing path is ${url.path}")
+        developers {
+            developer {
+                id.set("amommers")
+                name.set("Alexandre Mommers")
             }
+        }
+
+        scm {
+            connection.set("scm:git:https://github.com/wgpu4k/webgpu-ktypes.git")
+            developerConnection.set("scm:git:https://github.com/wgpu4k/webgpu-ktypes.git")
+            url.set("https://github.com/wgpu4k/webgpu-ktypes")
         }
     }
 }
-
-if (!isSnapshot()) {
-    val signingTasks = tasks.withType<Sign>()
-    tasks.withType<AbstractPublishToMaven>().configureEach {
-        dependsOn(signingTasks)
-    }
-}
-
-/**
- * return false if SNAPSHOT is set to "false" else true
- */
-fun isSnapshot(): Boolean =
-    System.getenv("SNAPSHOT")?.equals("false")?.not() ?: true
-
